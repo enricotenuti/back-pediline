25a83244a8edf0d8afbd403657942de44b347a26
enricotenuti <tenutz_27@outlook.it>
2024-06-11 15:35:58 +0200

    running html

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,10 @@ const path = require('path');
 app.use(cors({
     origin: 'http://localhost:10000',
     credentials: true
-  }));
-  app.use(cookieParser());
+}));
+app.use(cookieParser());
 
 const dotenv = require('dotenv').config({ path: path.join(__dirname, '.env') });
-
 
 const users = require('./users.js');
 const lines = require('./lines.js');
@@ -23,17 +22,15 @@ const alerts = require('./alerts.js');
 const authentication = require('./authentication.js');
 const tokenChecker = require('./tokenChecker.js');
 
-
 /**
  * Configure Express.js parsing middleware
  */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 /* Swagger */
-const swaggerJsDoc = require('swagger-jsdoc')
-const swaggerUi = require('swagger-ui-express')
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const swaggerOptions = {
     definition: {
         openapi: '3.0.0',
@@ -47,7 +44,7 @@ const swaggerOptions = {
 const swaggerDocument = swaggerJsDoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); // ? write in browser localhost:3000/api-docs
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
     console.log(req.method + ' ' + req.url)
     next()
 })
@@ -62,32 +59,9 @@ app.use('/api/v1/token', tokenChecker);
 app.use('/api/v1/authenticate', authentication);
 app.use('/api/v1/alerts', alerts);
 
-const vueDistPath = path.join(__dirname, '../dist');
-app.use(express.static(vueDistPath));
-
 app.get('*', (req, res) => {
-    res.sendFile(path.join(vueDistPath, 'index.html'));
+    res.sendFile(path.join(__dirname, '../index.html')); // Use path.join to get the absolute path
 });
-
-// const { auth } = require('express-openid-connect');
-
-// const config = {
-//   authRequired: false,
-//   auth0Logout: true,
-//   secret: process.env.AUTH_SECRET,
-//   baseURL: 'http://localhost:3000',
-//   clientID: process.env.AUTH_CLIENT_ID,
-//   issuerBaseURL: 'https://dev-z7rq7o4n5qxdrkm2.us.auth0.com'
-// };
-
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-// app.use(auth(config));
-
-// req.isAuthenticated is provided from the auth router
-// app.get('/', (req, res) => {
-//   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-// });
-
 
 /* Default 404 handler */
 app.use((req, res) => {
